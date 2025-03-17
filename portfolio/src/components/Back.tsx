d48ed6363bef0fe8d6d1856405c39d3366485598
piyush choudhary <piyushchoudhary.068@gmail.com>
2025-03-17 15:22:26 +0530

    fix z index

--- portfolio/src/components/Back.tsx
+++ portfolio/src/components/Back.tsx
@@ -8,10 +8,10 @@ export default function Back(){
         animate={{y:0}}
         transition={{ease:"easeIn", duration:0.5,delay:0.5}}
         
-        className=" fixed w-full px-12 py-5  z-50">
+        className=" fixed w-full px-12 py-5  z-100">
              <Link to="/"> <div className="flex  gap-1 items-center">
 
- <div className="bg-white w-16 px-3 py-1  h-8 font-secondary scale-y-120 italic rounded-3xl flex justify-center items-center">
+ <div className="bg-white  w-16 px-3 py-1  h-8 font-secondary scale-y-120 italic rounded-3xl flex justify-center items-center">
 
  
 <div className="text-lg scale-x-125"><LiaLongArrowAltLeftSolid />
